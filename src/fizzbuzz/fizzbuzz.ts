f60ba4e73bd759c968428ec4935c0c10f710be0d
codingMechanic73 <alanwinston2019@gmail.com>
2023-01-06 21:21:53 +0530

    fizzbuzz using map

--- src/fizzbuzz/fizzbuzz.ts
+++ src/fizzbuzz/fizzbuzz.ts
@@ -1,3 +1,10 @@
+/**
+ * Generate FizzBuzz using Simple for loop
+ * TimeComplexity: O(n)
+ * SpaceComplexity: O(n)
+ * @param n array length
+ * @returns FizzBuzz array
+ */
 export const generateFizzBuzz = (n: number) => {
     const array = [];
     for (let i = 1; i <= n; i++) {
@@ -12,4 +19,27 @@ export const generateFizzBuzz = (n: number) => {
         array.push(result);
     }
     return array;
+}
+
+
+/**
+ * Generate FizzBuzz using Simple for loop
+ * TimeComplexity: O(n)
+ * SpaceComplexity: O(n)
+ * @param n array length
+ * @returns FizzBuzz array
+ */
+export const generateFizzBuzzWithReduce = (n: number) => {
+    return Array.from({length: n}).map((_, index) => {
+        if (index % 15 == 0) {
+            return "FizzBuzz";
+        }
+        if (index % 3 == 0) {
+            return "Fizz";
+        }
+        if (index % 5 == 0) {
+            return "Buzz";
+        }
+        return index.toString();
+    });
 }
--- test/fizzbuzz/fizzbuzz.test.ts
+++ test/fizzbuzz/fizzbuzz.test.ts
@@ -4,4 +4,8 @@ describe("Test fizzbuzz", () => {
     it("should generate fizzbuzz array", () => {
         expect(generateFizzBuzz(5)).toStrictEqual(["1", "2", "Fizz", "4", "Buzz"]);
     });
+
+    it("should generate fizzbuzz array using map", () => {
+        expect(generateFizzBuzz(5)).toStrictEqual(["1", "2", "Fizz", "4", "Buzz"]);
+    });
 })
